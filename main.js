1f9a1c629f0016d7e425d04d6c1151ce30fe7e0b
maayan2j <maayan2j@gmail.com>
2020-04-25 22:53:03 +0300

    Office

--- main.js
+++ main.js
@@ -1,2 +1,78 @@
 //write your code here to make the tests pass
 
+class Document{
+
+    constructor(name){
+            EmployeeName = this.name;
+    }
+
+}
+
+class Employee {
+
+    constructor(eName,docs){
+        employeeName = this.eName;
+
+    }
+    workOffice(){
+        //officeDocArray.push(this.docs)
+    }
+}
+
+
+class Manager {
+
+    constructor(employees,managerName){
+
+        employeesArray = this.employees;
+        manager = this.managerName;
+    }
+    hireEmployee(newEmployee){
+        this.employee = newEmployee
+        newEmployee.push(employeesArray)
+    }
+
+    askEmployeesToWork(){
+        screen;
+    }
+
+}
+
+class Cleaner {
+
+    constructor(cleanerName)
+        {
+                cleaner = cleanerName;
+        }
+
+        isClean(){
+            console.log("Clean")
+        }
+}
+
+class Office{
+
+    constructor(documents,managers,cleaners)
+    {
+    documentsArray = this.documents
+    managerArray = this.managers
+    cleanersArray = this.cleaners
+    }
+
+    hireCleaner(){
+
+        officeDocArray.push(cleanersArray)
+
+    }
+
+
+    hireManager(manager){
+        
+            newManager = []
+            manager.push(newManager)
+
+    }
+
+    startWorkDay(){}
+
+}
